fcef8d4eca43f98a8f2ce23d39633e4594f3e3a3
cedrickewi <cedrickewi@gmail.com>
2026-03-24 05:27:20 +0100

    web testing

---
+++ basic_webapp/cmd/web/handlers.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"path"
+)
+
+var pathToTemplates = "./templates/"
+
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "home.page.gohtml", nil)
+}
+
+type TemplateData struct {
+	IP   string
+	Data map[string]any
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, t string, data *TemplateData) error {
+	// parse the template from disk
+	parsedTemplate, err := template.ParseFiles(path.Join(pathToTemplates, t))
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return err
+	}
+
+	if data == nil {
+		data = &TemplateData{
+			Data: make(map[string]any),
+		}
+	}
+
+	data.IP = app.ipFromContext(r.Context())
+
+	//execute template
+	err = parsedTemplate.Execute(w, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- primeapp/main.go
+++ primeapp/main.go
@@ -99,3 +99,5 @@ func isPrime(n int) (bool, string) {
 	return true, fmt.Sprintf("%d is a prime number!", n)
 
 }
+
+
